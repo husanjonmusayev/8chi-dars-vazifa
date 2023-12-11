4caf2a3e67ff8371bdf717a98b6b0ad5857a921b
husanjonmusayev <husanjonm279@gmail.com>
2023-12-11 14:13:56 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,110 @@
+
+// function sum (arg){
+//     let max = 0
+//     for(let i = 0;  i < arg.length; i++ ){
+//         max += arg[i]
+//     }
+
+//     return max
+// }
+// console.log(sum([1,2,3,4,5]));
+
+// const arr = [1,2,3,4,5,6]
+// sum = 0;
+// for (const item of arr) {
+//     sum += item;
+// }
+// console.log(sum);
+
+// const user = {
+//     name: "jhon",
+//     age: 25,
+//     email: "test@gmail.com",
+
+
+
+// }
+// user.name = "doe"
+// delete user.name
+
+// vazifa
+// 1 masala
+
+
+// function mass(arg){
+//     let sum = 1;
+//     for(let i = 0; i < arg.length; i++){
+//     if(arg[i]%3==0 || arg[i]%5==0){
+//         sum*=arg[i]
+
+//     }   
+// }
+// return sum
+// }
+// console.log(mass([1,2,3,4,5,6,7,8,9,10]));
+
+
+//  2 masala
+
+// function mass(arg) {
+//     let newMass = [ ];
+//     for (let i = 0; i < arg.length; i++) {
+//         newMass.push (arg[i][0]);
+//     }
+//     return newMass;
+// }
+// console.log(mass(['sardor' , 'donior']));
+
+//  3 masala
+// function mass(arg) {
+//      let sum = [ ];
+//     for (let i = 0; i < arg.length; i++) {
+       
+//         if (arg[i] % 2 == 1) {
+//           sum.push (arg[i]);  
+//         }
+//     }
+//     return sum;  
+// }
+
+// console.log(mass([1,2,3,4,5,6,7,8,9,10]));
+
+//  4 masala
+// function mass(arg) {
+//     let caunt = 0;
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 1) {
+//             caunt++;
+//         }
+//     }
+//     return caunt;
+// }
+
+// console.log(mass([1,2,3,4,5,6,7,8,9,10]));
+
+//  5 masala
+// function mass(arg) {
+//     let newMass = [ ]; 
+//     for (let i = 0; i<arg.length; i++){
+//         if((arg[i]) >= arg[i]){
+//     newMass.push(arg[i]);
+//         }
+//     }
+//     return newMass;
+// }
+// console.log(mass(['samandar','dior', 'jamshid']));
+
+//  7 masala
+// function mass(arg){
+//     let sum = 1;
+//     for(let i = 0; i < arg.length; i++){
+//     if(arg[i]%3==0 && arg[i]%7==0){
+//         sum*=arg[i]
+
+//     }   
+// }
+// return sum
+// }
+// console.log(mass([1,2,3,4,5,6,7,8,9,10]));
+
+
